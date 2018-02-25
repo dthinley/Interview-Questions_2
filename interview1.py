afd48826fbec96b5ef75c3d70ffbc29d1762286d
Dorjee Thinlay <dthinley@yahoo.com>
2018-02-25 11:28:38 -0800

    Update interview1.py

--- interview1.py
+++ interview1.py
@@ -1,20 +1,19 @@
-def getCharCount(s):
-	charCount_s = {}
-	for char in s:
-		if char not in charCount_s:
-			charCount_s[char] = 1
-		else:
-			charCount_s[char] += 1
-	return charCount_s
+# Check if s1 and s2 are anagram to each other
+def anagram_check(s1, s2):
+    # sorted returns a new list and compare
+    return sorted(s1) == sorted(s2)
 
-def anagramTest(s, t):
-	if type(s) != str or type(t) != str or len(s) == 0 or len(s) != len(t):
-		#print('False: input error')
-		return False
-	else:
-		charCount_s = getCharCount(s)
-		charCount_t = getCharCount(t)
-		if charCount_s == charCount_t:
-			print('True: '+s+" is an anagram of "+t)
-			return True
-		return False
+# Check if anagram of t is a substring of s
+def question1(s, t):
+    for i in range(len(s) - len(t) + 1):
+        if anagram_check(s[i: i+len(t)], t):
+            return True
+    return False
+
+def main():
+    print (question1("udacity", "city"))
+    print (question1("dorjee", "ee"))
+    print (question1("dorjee", "aa"))
+
+if __name__ == '__main__':
+    main()  
