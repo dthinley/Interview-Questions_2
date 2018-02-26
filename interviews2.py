a00ad0e9919dee467d398223633c05ba7059a993
Dorjee Thinlay <dthinley@yahoo.com>
2018-02-26 12:18:48 -0800

    Update interviews2.py

--- interviews2.py
+++ interviews2.py
@@ -1,26 +1,17 @@
-### Question 2 main function and helper functions.
-"""Given a string a, find the longest palindromic substring contained in a.
-Your function definition should look like question2(a), and return a string."""
-# Gives substrings of s in decending order.
-def substrings(s):
+def question2(a):
+    if len(a) <= 1:
+        return a
 
-    # Declare local variable for the length of s.
-    l = len(s)
-
-    # choose range over x-range for python version compatibility.
-    for end in range(l, 0, -1):
-        for i in range(l-end+1):
-            yield s[i: i+end]
-
-# Define palindrome.
-def palindrome(s):
-    return s == s[::-1]
-
-# Main function.
-def Question2(a):
-    for l in substrings(a):
-        if palindrome(l):
-            return l
+    longest = ""
+    for i in range(len(a)):
+        for j in range(0, i):
+            substring = a[j:i+1]
+            if substring == substring[::-1]:
+                if len(substring) > len(longest):
+                    longest = substring
+    if len(longest) == 0:
+        return a[0]
+    return longest
 
 # Test case.
-print (Question2("racecar"))
+print (question2("raca"))
